fe83f3a4e773242da376902eef9dfe65bd10eab0
wshi27 <wshi27@hotmail.com>
2023-08-17 22:53:10 -0400

    Revert "added weather api"
    This reverts commit 2132317b40af5e0eef2363973d0b2d9feb1bf7a2.

--- App.js
+++ App.js
@@ -1,34 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [apiResponse, setApiResponse] = useState("");
-  useEffect(() => {
-    async function api_content() {
-      const response = await fetch(
-        // Make this YOUR URL
-        "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/11355?unitGroup=metric&key=PJY051BB1KI1ZD9044U4MPM2P&contentType=json",
-        {}
-      );
-      const json = await response.json();
-      return json;
-    }
-
-    api_content().then((days) => {
-      console.log(days);
-      setApiResponse(JSON.parse(days));
-    });
-  }, []);
   return (
     <View style={styles.container}>
-      <Text>welcome to the Forecast Frequencies app!!</Text>
-      {/* {apiResponse?.days((daily) => (
-        <>
-          <Text>{daily.datetime}</Text> <Text>{daily.temp}</Text>
-        </>
-      ))} */}
-      <Text>{apiResponse?.address}</Text>
+      <Text>welcome to the Forecast Frequencies app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -37,8 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
