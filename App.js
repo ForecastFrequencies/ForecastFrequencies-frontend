48c2a06488cd72a89f4f75e8a7b6cf46517fc506
wshi27 <wshi27@hotmail.com>
2023-09-02 03:47:23 -0400

    added folders

--- App.js
+++ App.js
@@ -1,26 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { PaperProvider, Button } from 'react-native-paper';
+import Login from './src/components/login/Login';
 
 export default function App() {
+  const []
   
   return (
     <PaperProvider>
       <View style={styles.container}>
         <Text>Welcome to the Forecast Frequencies app!</Text>
+        <Login/>
         <Button
           icon="camera"
           mode="contained"
           onPress={() => console.log('Pressed')}
         >
-          Press me
+          Home screen
         </Button>
         <StatusBar style="auto" />
      
       </View>
-  
-
     </PaperProvider>
   );
 }
@@ -28,8 +29,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
