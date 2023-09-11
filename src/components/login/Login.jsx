2e542ec1100b07fcb59a27a378e98c8a0cdee622
wshi27 <wshi27@hotmail.com>
2023-09-11 06:56:23 -0400

    added navigation and parts of homepage

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,11 +1,21 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { PaperProvider, Button } from 'react-native-paper';
 
-const Login = () => {
+const Login = ({ navigation }) => {
     return (
-        <View>
-            <Text>Login</Text>
-        </View>
+        <PaperProvider>
+            <View >
+                <Text>This is the Login screen</Text>
+                <Button
+                    icon="camera"
+                    mode="contained"
+                    onPress={() => navigation.navigate('Home')}
+                >
+                    Home screen
+                </Button>
+            </View>
+        </PaperProvider>
     )
 }
 
